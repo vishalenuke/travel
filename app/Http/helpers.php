e7fd7b804955948a74cce087c0b083c817147f63
Mayank <mayank.pratap@enukesoftware.com>
2017-01-03 14:44:23 +0530

    updated

--- app/Http/helpers.php
+++ app/Http/helpers.php
@@ -50,7 +50,7 @@ function user_Email( $email,$password='',$message='Your account is created with 
 
 	try{
 		if( !empty($email))
-		return Mail::send('emails.notification', ['email'=>$email,'password'=>$password,'token'=> base64_encode($email),'message'=>$message], function ($m) use($email)   {
+		return Mail::send('emails.notification', ['email'=>$email,'password'=>$password,'token'=> base64_encode($email),'message'=>$details], function ($m) use($email)   {
 								
 					            $m->from('contact@travels.com', 'Travel Portal');
 
@@ -60,7 +60,7 @@ function user_Email( $email,$password='',$message='Your account is created with 
 	}catch(\Exception $e){
 
 	}
-	return 0;
+	
 
 }
 ?>
