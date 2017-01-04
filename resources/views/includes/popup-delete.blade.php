3d4274e8e537305b1589b16330570b3bfdf986f9
Mayank <mayank.pratap@enukesoftware.com>
2017-01-04 13:11:00 +0530

    updated

---
+++ resources/views/includes/popup-delete.blade.php
@@ -0,0 +1,27 @@
+<div class="forgot_password">
+			<div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel">
+			  <div class="modal-dialog" role="document">
+				<div class="modal-content">
+				  <div class="modal-header">
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+					<h4 class="modal-title" id="deleteModalLabel">Confirm</h4>
+				  </div>
+				  <div class="modal-body">
+							<center>
+								
+									<h4><div id="confirm_message">Do you really want to delete.</div></h5>
+															
+							  <br/>
+							 <button type="cancel" class="btn btn-reset " data-dismiss="modal" aria-label="Close">Cancel</button>
+							  <button type="button" class="btn btn-reset" id="delete_popup">Delete</button>
+							</center>
+								
+							  
+							
+				  </div>
+				 
+				</div>
+			  </div>
+			</div>
+		</div>
+
